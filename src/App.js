e279174355136c58fa6c4708bcc971bf8be14398
kececirecep <kececirecep@outlook.com>
2023-11-21 00:46:31 +0300

    project-upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [joke, setJoke] = useState();
+
+
+  const randomJoke = async () => {
+    try {
+      const response = await axios.get("https://official-joke-api.appspot.com/random_joke")
+      setJoke(response.data)
+    } catch (error) {
+      console.error(error)
+    }
+  };
+
+  useEffect(() => {
+    randomJoke()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-500 h-screen w-screen flex flex-col justify-center items-center">
+      <h1 className='text-3xl'>{joke?.setup}</h1>
+      <h1 className='text-2xl'>{joke?.punchline}</h1>
+      <button className='bg-blue-600 hover:bg-blue-700 mt-10 text-white font-bold py-2 px-4 rounded' onClick={() => randomJoke()}>Random Joke</button>
     </div>
   );
 }
